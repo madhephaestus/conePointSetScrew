c6a4350f88fe4ec0d243c54cfc21024f27f82ef7
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-19 13:09:02 -0400

    cone point

--- conePointSetScrew.groovy
+++ conePointSetScrew.groovy
@@ -23,12 +23,12 @@ import com.google.gson.reflect.TypeToken;
 	
 import eu.mihosoft.vrl.v3d.parametrics.*;
 CSG getNut(){
-	String type= "conePointSetScrew"
+	String type= "PhillipsRoundedHeadThreadFormingScrews"
 	LengthParameter facets		= new LengthParameter("Bolt Hole Facet Count",10,[40,10])
 	LengthParameter boltLength		= new LengthParameter("Bolt Length",10,[180,10])
 	LengthParameter offset		= new LengthParameter("printerOffset",0.0,[2,0])
 	if(args==null)
-		args=["M3x8"]
+		args=["M3x6"]
 	String sizeVar = args.get(0)
 	StringParameter size = new StringParameter(	type+" Default",
 										sizeVar,
@@ -38,21 +38,17 @@ CSG getNut(){
 	//println Vitamins.listVitaminSizes(type)
 	HashMap<String,Object> servoConfig = Vitamins.getConfiguration( type,size.getStrValue())
 	//println servoConfig
-	headDiameter=Double.parseDouble(servoConfig.get("headDiameter").toString())+offset.getMM()
-	headHeight=Double.parseDouble(servoConfig.get("headHeight").toString())
 	keyDepth=Double.parseDouble(servoConfig.get("keyDepth").toString())
 	keySize=Double.parseDouble(servoConfig.get("keySize").toString())
-	outerDiameter=Double.parseDouble(servoConfig.get("drillDiameter").toString())+offset.getMM()
+	outerDiameter=Double.parseDouble(servoConfig.get("outerDiameter").toString())+offset.getMM()
 	if(servoConfig.get("boltLength")!=null)
 		boltLength.setMM(servoConfig.get("boltLength"))
 
 	
-	CSG head =new Cylinder(headDiameter/2,headDiameter/2,headHeight,(int)facets.getMM()).toCSG() // a one line Cylinder
-				.toZMin()
 	CSG shaft =new Cylinder(outerDiameter/2,outerDiameter/2,boltLength.getMM(),(int)facets.getMM()).toCSG() // a one line Cylinder
 				.toZMax()
 	
-	return head.union(shaft)
+	return shaft
 		.setParameter(size)
 		.setParameter(boltLength)
 		.setParameter(facets)
